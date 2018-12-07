9a9c02395f088361e832e16722d613634393b643
arunabhcode <arunabh.1990@gmail.com>
2018-12-07 00:28:40 -0500

    realsense minimal example and cmake compile options fix

--- apps/RealsenseMinimalExample/src/RealsenseMinimalExample.cpp
+++ apps/RealsenseMinimalExample/src/RealsenseMinimalExample.cpp
@@ -1,8 +1,22 @@
 // Copyright 2018 Arunabh Sharma
 
 #include <iostream>
+#include <string>
+
+#include "librealsense2/rs.hpp"
 
 int main()
 {
+    rs2::context ctx;
+
+    std::vector<rs2::sensor> list = ctx.query_all_sensors();
+
+    // std::string my_type = typeid(list).name();
+    // std::cout << system(("echo " + my_type + " | c++filt -t").c_str()) << std::endl;
+
+    if (list.size() > 0)
+    {
+        std::cout << "Device : " << list[0] << std::endl;
+    }
     return 0;
 }
