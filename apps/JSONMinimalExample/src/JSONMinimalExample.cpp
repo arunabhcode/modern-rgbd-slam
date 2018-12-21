cf8d46c4191083d06781cabe4203e37eb3743c20
arunabhcode <arunabh.1990@gmail.com>
2018-12-21 07:34:22 +0530

    json minimal example

---
+++ apps/JSONMinimalExample/src/JSONMinimalExample.cpp
@@ -0,0 +1,28 @@
+// Copyright 2018 Arunabh Sharma
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "nlohmann/json.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+	std::cout << "Expected command: <name of executable> <name of json>" << std::endl;
+        return 0;
+    }
+    nlohmann::json j;
+
+    std::ifstream in_file(argv[1], std::ios::in | std::ios::binary);
+    if(!in_file.is_open())
+    {
+	std::cout << "Unable to open file" << std::endl;
+	return 0;
+    }	
+    
+    in_file >> j;
+    std::cout << "Value of pi = " << j["pi"] << std::endl;		
+    return 0;
+}
