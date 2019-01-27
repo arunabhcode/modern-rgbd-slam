9eb053fbd66d597b40447433aa8d675226b48c8b
arunabhcode <arunabh.1990@gmail.com>
2019-01-26 19:48:02 -0500

    eigen minimal example

---
+++ apps/EigenMinimalExample/src/EigenMinimalExample.cpp
@@ -0,0 +1,12 @@
+// Copyright 2018 Arunabh Sharma
+
+#include <iostream>
+
+#include "Eigen/Core"
+
+int main(int argc, char** argv)
+{
+    Eigen::Matrix3f identity = Eigen::Matrix3f::Identity(3, 3);
+    std::cout << "Identity 3x3 Matrix = \n" << identity << std::endl;
+    return 0;
+}
